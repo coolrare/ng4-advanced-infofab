39475e0042a0ff2a222edfee607f1060cc2cc34d
Will保哥 <doggy.huang@gmail.com>
2017-05-23 09:36:49 +0800

    加入 page1 與 page2 元件並設定好路由機制

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {Page1Component} from 'app/page1/page1.component';
+import {Page2Component} from 'app/page2/page2.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: 'page1', component: Page1Component},
+  {path: 'page2', component: Page2Component}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+@NgModule({imports: [RouterModule.forRoot(routes)], exports: [RouterModule]})
+export class AppRoutingModule {
+}
