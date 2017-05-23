dc6ebd09a5fdbca8a9849a35bb2ab59645967028
Will保哥 <doggy.huang@gmail.com>
2017-05-23 11:32:53 +0800

    套用 /dashboard 與 /cards 版面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import {NgModule} from '@angular/core';
 import {Route, RouterModule, Routes} from '@angular/router';
-import {Page1Component} from 'app/page1/page1.component';
-import {Page2Component} from 'app/page2/page2.component';
+import {CardsComponent} from 'app/cards/cards.component';
+import {DashboardComponent} from 'app/dashboard/dashboard.component';
 import {fallbackRoute} from 'app/shared/fallback-route';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/page1', pathMatch: 'full'},
-  {path: 'page1', component: Page1Component, data: { title: 'Page 1' }},
-  {path: 'page2/a/b/c/d', component: Page2Component}, fallbackRoute
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent, data: {title: 'Page 1'}},
+  {path: 'cards', component: CardsComponent}, fallbackRoute
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: false,
-    enableTracing: false
-  })],
+  imports:
+      [RouterModule.forRoot(routes, {useHash: false, enableTracing: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
