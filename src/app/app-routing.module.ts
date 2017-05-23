070d7ffe1136f55b9ed0c036a1c6faa380f5aac2
Will保哥 <doggy.huang@gmail.com>
2017-05-23 16:55:42 +0800

    在 /cards/:type 啟用 LoginGuard 路由守門員

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import {DashboardComponent} from 'app/dashboard/dashboard.component';
 import {fallbackRoute} from 'app/shared/fallback-route';
 import { LayoutComponent } from "app/layout/layout.component";
 import { LoginComponent } from "app/login/login.component";
+import { LoginGuard } from "app/login.guard";
 
 const routes: Routes = [
   {path: '', component: LayoutComponent,
   children: [
     {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: DashboardComponent, data: {title: 'Page 1'}},
-    {path: 'cards/:type', component: CardsComponent},
+    {path: 'cards/:type', component: CardsComponent,
+     canActivate: [
+       LoginGuard
+     ]
+    },
     {path: 'charts', loadChildren: './charts/charts.module#ChartsModule'},
   ]},
   {path: 'login', component: LoginComponent},
