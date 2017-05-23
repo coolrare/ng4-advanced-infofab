a0444d86218d9cd9f3167f31a7a0c8c20ead96d8
Will保哥 <doggy.huang@gmail.com>
2017-05-23 14:13:22 +0800

    在 cards 取得 :type 參數值

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -7,10 +7,21 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+
+  type = 0;
+
   constructor(private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // 直接取得當下的 :type 參數值
+    this.type = this.route.snapshot.params['type'];
+
+    // 透過 Observable 取得即時的 :type 參數值
+    this.route.params.subscribe((params) => {
+      this.type = params['type'];
+    });
   }
 
 }
