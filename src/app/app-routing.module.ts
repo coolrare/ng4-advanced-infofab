3066b4698079225bf243c002cf60631025753e4d
Will保哥 <doggy.huang@gmail.com>
2017-05-23 10:49:37 +0800

    將萬用路由獨立成一個變數，並標示 Route 型別

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {Route, RouterModule, Routes} from '@angular/router';
 import {Page1Component} from 'app/page1/page1.component';
 import {Page2Component} from 'app/page2/page2.component';
+
+const fallbackRoute: Route = {
+  path: '**', redirectTo: '/page1', pathMatch: 'full'
+};
 
 const routes: Routes = [
   {path: '', redirectTo: '/page1', pathMatch: 'full'},
   {path: 'page1', component: Page1Component},
   {path: 'page2/a/b/c/d', component: Page2Component},
-  {path: '**', redirectTo: '/page1', pathMatch: 'full'}
+  fallbackRoute
 ];
 
 @NgModule({
