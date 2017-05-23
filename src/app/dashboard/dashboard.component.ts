0fe6be90f790a1623e739ecc49eb6f7b0a6fb212
Will保哥 <doggy.huang@gmail.com>
2017-05-23 12:01:06 +0800

    修正 /dashboard 頁面中 jQuery 事件無法重新註冊的問題

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {initDashboard} from 'app/shared/init-dashboard';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    $(initDashboard);
   }
-
 }
