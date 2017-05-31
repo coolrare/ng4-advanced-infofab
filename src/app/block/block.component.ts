915112fcd99ea4ca7ca3213b1f9c293d3b75c3e6
Will保哥 <doggy.huang@gmail.com>
2017-05-31 16:40:00 +0800

    在 block 元件加上一個 title 屬性 (可繫結)

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-block',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./block.component.css']
 })
 export class BlockComponent implements OnInit {
+
+  @Input()
+  title = 'Sky red';
 
   constructor() { }
 
