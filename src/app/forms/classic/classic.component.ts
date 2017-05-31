e8eba9248d456e27ad626e44b06ca328041cf6e4
Will保哥 <doggy.huang@gmail.com>
2017-05-31 10:50:52 +0800

    示範發出 HTTP Post 要求到 JSON Server

--- src/app/forms/classic/classic.component.ts
+++ src/app/forms/classic/classic.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import { Observable } from "rxjs/Observable";
+import { Http, Headers, RequestOptions } from "@angular/http";
 
 @Component({
   selector: 'app-classic',
@@ -10,13 +12,23 @@ export class ClassicComponent implements OnInit {
 
   data: any = {};
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
   doSubmit(f: NgForm) {
     console.log(f.value);
+
+    let body = JSON.stringify(f.value);
+    let headers = new Headers({ 'Content-Type': 'application/json'});
+    let options = new RequestOptions({ headers: headers });
+
+    this.http.post('http://localhost:3000/myform', body, options)
+      .subscribe((res) => {
+        console.log(res);
+      });
+
   }
 
 }
