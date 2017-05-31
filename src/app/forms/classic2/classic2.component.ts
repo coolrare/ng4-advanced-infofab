ea120fa7a14f96cf6adbd8795481f00a5899f721
Will保哥 <doggy.huang@gmail.com>
2017-05-31 14:47:38 +0800

    示範在 FormGroup 下動態新增 FormControl 的方法

--- src/app/forms/classic2/classic2.component.ts
+++ src/app/forms/classic2/classic2.component.ts
@@ -24,11 +24,20 @@ export class Classic2Component implements OnInit {
       desc: ['Hello World',
         [Validators.required]
       ],
+      group1: this.fb.group({
+        sample1: this.fb.control('sample 1'),
+        sample2: this.fb.control('sample 2')
+      }),
       addresses: this.fb.array([
         this.fb.control('address 1'),
         this.fb.control('address 2')
       ])
     });
+  }
+
+  addSample() {
+    const group1 = this.form.controls.group1 as FormGroup;
+    group1.addControl('sample3', this.fb.control('sample 3'));
   }
 
   addAddress() {
