f1095bcbc998f95b94e0537391c05001929a8617
Will保哥 <doggy.huang@gmail.com>
2017-05-31 13:52:14 +0800

    建立 FormGroup 物件並在 Template 中使用 form 物件

--- src/app/forms/classic2/classic2.component.ts
+++ src/app/forms/classic2/classic2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-classic2',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Classic2Component implements OnInit {
 
-  constructor() { }
+  data: any = {
+      title: 'Will Huang',
+      desc: 'Hello World'
+    };
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      title: 'Will Huang',
+      desc: 'Hello World'
+    });
   }
 
 }
